9368401846d6727ce4513655e2ff77140ba32f46
sarallewelyn <sara.e.llewellyn@gmail.com>
2017-02-08 18:21:47 +0000

    Revert "4.7.2 dark element"
    This reverts commit 39ad084c0b382633d09076c6d45a45a93017e453.

--- wp-content/themes/darkelements/js/nav.js
+++ wp-content/themes/darkelements/js/nav.js
@@ -8,43 +8,43 @@
 
 jQuery(document).ready(function() { 
 	// build <select> dropdown
-	jQuery("<select />").appendTo("div.nav-primary");
+	jQuery("<select />").appendTo("div.nav-head");
 
 	// create option Menu
 	jQuery("<option />", {
 		"selected": "selected",
 		"value": "",
 		"text": objectL10n.navText  
-	}).appendTo(".nav-primary select");
+	}).appendTo(".nav-head select");
 
 	// populate
-	jQuery(".nav-primary ul li a").each(function() {
+	jQuery(".nav-head ul li a").each(function() {
 		var el = jQuery(this);
-		if(el.parents(".nav-primary ul ul ul").length) {
+		if(el.parents(".nav-head ul ul ul").length) {
 			// if subpage level two
 			jQuery("<option />", {
 				"value": el.attr("href"),
 				"text":  "- - " + el.text()
-			}).appendTo(".nav-primary select");
+			}).appendTo(".nav-head select");
 		} 
-		else if(el.parents(".nav-primary ul ul").length) {
+		else if(el.parents(".nav-head ul ul").length) {
 			// if subpage level one
 			jQuery("<option />", {
 				"value": el.attr("href"),
 				"text":  "- " + el.text()
-			}).appendTo(".nav-primary select");
+			}).appendTo(".nav-head select");
 		} 
 		else { 
 			// if no subpage
 			jQuery("<option />", {
 				"value": el.attr("href"),
 				"text": el.text()
-			}).appendTo(".nav-primary select");
+			}).appendTo(".nav-head select");
 		}
 	});
 
 	// make links work 
-	jQuery(".nav-primary select").change(function() { 
+	jQuery(".nav-head select").change(function() { 
 		window.location = jQuery(this).find("option:selected").val();
 	});
 });
